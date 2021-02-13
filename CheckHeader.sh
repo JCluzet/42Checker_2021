e1aa7d63fe127e24d3f298b57e833b0088f5cd99
JCluzet <55356071+JCluzet@users.noreply.github.com>
2021-02-13 21:12:00 +0100

    BetaTest

--- CheckHeader.sh
+++ CheckHeader.sh
@@ -6,7 +6,7 @@
 #    By: jcluzet <jo@cluzet.fr>                     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/13 00:44:50 by jcluzet           #+#    #+#              #
-#    Updated: 2021/02/13 21:10:23 by jocluzet         ###   ########.fr        #
+#    Updated: 2021/02/13 21:11:59 by jocluzet         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,7 +26,7 @@ cd ~/.42Checker_2021/;
 		
 	git fetch origin
 	version=$(git reset --hard | cut -c30-)
-	printf "   ${blanc}Version 42Checker_2021 : ${vertclair}${version} > "
+	printf "   ${blanc}Version 42Checker_2021 : ${vertfonce}${version} ${neutre}> ${vertclair}"
 	git pull
 
 cd $@;
